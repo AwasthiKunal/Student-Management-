f9e785b7c7d201046922634728d28055d319b28a
AwasthiKunal <kunalawasthi029@gmail.com>
2023-12-05 13:54:13 +0530

    First Commit

---
+++ results/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import DeclareResult
+admin.site.register(DeclareResult)
---
+++ student_classes/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import StudentClass
+admin.site.register(StudentClass)
